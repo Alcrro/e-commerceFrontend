0ee5540626320368d7a0326224ab3f75d7a39e74
alcrro <alex.roventa94@gmail.com>
2025-05-03 17:16:10 +0300

    Add “Thanks for Being With Us” Section. Closes #49

---
+++ src/components/profile/main/myaccount/history/MyAccountHistory.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './myAccountHistory.module.scss';
+const MyAccountHistory = () => {
+  return (
+    <div className={style.my_account_history}>
+      <div className={style.text}>Thx for join us</div>
+    </div>
+  );
+};
+
+export default MyAccountHistory;
