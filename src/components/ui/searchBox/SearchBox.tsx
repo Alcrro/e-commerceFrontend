85afe7fe303888019c9fc15ce86a4b3d3838156a
alcrro <alex.roventa94@gmail.com>
2025-05-17 00:10:10 +0300

    Add search box ui. Closes #62

--- src/components/ui/searchBox/SearchBox.tsx
+++ src/components/ui/searchBox/SearchBox.tsx
@@ -1,23 +1,101 @@
-import React, { ReactNode } from 'react';
+'use client';
+import React, { useState, useTransition, useEffect } from 'react';
 import style from './searchBox.module.scss';
 import SearchInput from './SearchInput';
-import CustomIcon from '../icon/Icon';
 import ProductListFounded from './ProductListFounded';
+import { useToggleSwitch } from '@/store/toggleSwitch';
+import { serverServices } from '@/service/serverAction/serverServices';
+import DeleteButton from '../buttons/deleteButton/DeleteButton';
+import SearchButton from '../buttons/searchButon/SearchButton';
+
 const SearchBox = () => {
+  const isToggled = useToggleSwitch((state) => state.isToggled);
+
+  const [products, setProducts] = useState<IApiResponse<IBaseProduct[]>>({
+    success: false,
+    message: '',
+    data: [],
+  });
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isPending, startTransition] = useTransition();
+
+  const handleSearchClick = () => {
+    if (searchTerm.trim() === '') {
+      setProducts({
+        success: false,
+        message: '',
+        data: [],
+      });
+      return;
+    }
+
+    startTransition(async () => {
+      const results = await serverServices.searchProduct(searchTerm);
+      if (!results) {
+        setProducts({
+          success: false,
+          message: '',
+          data: [],
+        });
+      } else {
+        setProducts(results);
+      }
+    });
+  };
+
   return (
-    <div className={`${style.search_box_container} w-full relative mx-3`}>
+    <div
+      className={`${!isToggled && 'hidden'} max-lg:absolute z-40 max-lg:left-[50%] max-lg:translate-x-[-50%] max-lg:mx-0 rounded-none top-full lg:relative lg:block ${style.search_box_container} w-full mx-3`}
+      onClick={(e) => e.stopPropagation()}
+    >
       <SearchInput
-        classname={'flex items-center w-full relative'}
+        classname={'flex items-center relative rounded-2xl group'}
+        value={searchTerm}
+        onChangeHandler={(value: string) => setSearchTerm(value)}
+        onKeyDown={handleSearchClick}
         icon={
-          <CustomIcon
-            iconUnicode="\F52A"
-            srcIsEnabled={false}
-            classname="absolute z-10 right-3 text-gray-400 group-focus-within:text-blue-400 transition-colors"
-          />
+          <div
+            className="flex gap-2 items-center absolute right-0 z-[99999!important] h-full"
+            onClick={(e) => e.stopPropagation()}
+          >
+            {searchTerm && (
+              <div
+                className="text-white group-focus-within:text-black"
+                onMouseDown={(e) => e.preventDefault()}
+              >
+                <DeleteButton
+                  onClick={() => {
+                    setSearchTerm('');
+                    setProducts({
+                      success: false,
+                      message: '',
+                      data: [],
+                    });
+                  }}
+                />
+              </div>
+            )}
+            <div
+              className="bg-slate-400 w-12 h-full flex justify-center lg:rounded-r-2xl group-focus-within:rounded-b-none text-white"
+              onMouseDown={(e) => e.preventDefault()}
+              onClick={() => handleSearchClick()}
+            >
+              <SearchButton classname="" />
+            </div>
+          </div>
         }
       />
-      <div className={`${style.products_list_founded}`} tabIndex={-1}>
-        <ProductListFounded />
+
+      <div
+        className={`${style.products_list_founded}`}
+        tabIndex={-1}
+        onMouseDown={(e) => e.preventDefault()}
+      >
+        <ProductListFounded products={products.data} />
+        {isPending && (
+          <div className="p-2 text-sm text-gray-500">Loading...</div>
+        )}
       </div>
     </div>
   );
--- src/components/ui/searchBox/SearchInput.tsx
+++ src/components/ui/searchBox/SearchInput.tsx
@@ -3,17 +3,32 @@ import style from './searchInput.module.scss';
 const SearchInput = ({
   icon,
   classname,
+  onChangeHandler,
+  onKeyDown,
+  value,
 }: {
   icon?: ReactNode;
   classname?: string;
+  onChangeHandler: (value: string) => void;
+  onKeyDown: () => void;
+  value: string;
 }) => {
   return (
     <div className={`group ${style.search_box_inner} ${classname}`}>
       <input
         type="text"
-        className=" focus-within:bg-blue-100 focus-visible:outline-none focus-within:border-none"
+        className=" focus-within:bg-blue-100 focus-within:text-black text-[var(--text-container-color)] focus-visible:outline-none focus-within:border-none lg:rounded-2xl"
         placeholder="Search product"
+        value={value}
+        name="value"
+        onChange={(e) => onChangeHandler(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            onKeyDown();
+          }
+        }}
       />
+
       {icon}
     </div>
   );
